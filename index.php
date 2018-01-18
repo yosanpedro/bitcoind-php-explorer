49d09ee2d8d06a15060ceec11e9ccbf7331922ad
jim <nobody@here>
2018-01-18 22:50:05 +0700

    top 20 and total issued done

--- index.php
+++ index.php
@@ -1,16 +1,19 @@
 <html lang="en">
 <head>
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-    <title>Document</title><style type='text/css'>
-      tbody {
-                        background-color: #c1c2ed;
-                     }
-      .hdr {
+    <title>Document</title>
+    <style type='text/css'>
+        tbody {
+            background-color: #c1c2ed;
+        }
+
+        .hdr {
             background-color: #FFEB3B;
             box-shadow: inset -2px 2px 5px grey;
         }
-        .data{
-            background-color: gainsboro ;
+
+        .data {
+            background-color: gainsboro;
             box-shadow: inset -2px 2px 5px grey;
         }
     </style>
@@ -20,9 +23,8 @@
 $_BLOCKCHAIND = "~/stackbitd";
 $qry = [];
 parse_str($_SERVER['QUERY_STRING'], $qry);
-
-//var_dump($qry);
-
+global $_REURL;
+$_REURL = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 if (isset($qry["block"])) {
     $useblock = $qry["block"];
 }
@@ -32,34 +34,44 @@ if (isset($qry["height"])) {
 if (isset($qry["tx"])) {
     $usetx = $qry["tx"];
 }
-
 if (isset($qry["address"])) {
     $useaddress = $qry["address"];
 }
+if (isset($qry["top20"])) {
+    $usetop20 = $qry["top20"];
+}
+if (isset($qry["total"])) {
+    $usetotal= $qry["total"];
+}
+if (NULL != $usetotal) {
+    $input =  json_decode(
+         '{ "as of": "'. date(DATE_ISO8601).'", "coins": ' . json_decode(`curl "http://localhost:5984/sbit_blocks/_design/address/_view/value?reduce=true&grouping=none" `)->rows[0]->value  . "}");
 
-if(NULL!= $useaddress){
-$amt=json_decode( `curl "http://localhost:5984/sbit_blocks/_design/address/_view/value?reduce=true&key=%22$useaddress%22" `);
-$cnt=json_decode( `curl "http://localhost:5984/sbit_blocks/_design/address/_view/counter?group=true&reduce=true&key=%22$useaddress%22" `);
+} else
+if (NULL != $usetop20) {
+    $input = json_decode(`./top20.sh`);
+} else
+    if (NULL != $useaddress) {
+        $amt = json_decode(`curl "http://localhost:5984/sbit_blocks/_design/address/_view/value?reduce=true&key=%22$useaddress%22" `);
+        $cnt = json_decode(`curl "http://localhost:5984/sbit_blocks/_design/address/_view/counter?group=true&reduce=true&key=%22$useaddress%22" `);
 //$first=`jq    .rows[].value < <(curl "http://localhost:5984/sbit_blocks/_design/address/_view/counter?group=true&reduce=true&key=%22$useaddress%22")`;
- echo "the address ${useaddress} has obtained/retained roughly ". ${amt}->rows[0]->value ." in ". ${cnt}->rows[0]->value ." transactions";
- exit(0);
+        $input=[ "${useaddress}"=> ${$amt}->rows[0]->value ,
+           "transactions" => ${cnt}->rows[0]->value ];
 
-}
-else if (NULL != $usetx) {
-    $json = `${_BLOCKCHAIND} gettransaction ${usetx}`;
-} else {
-    if (NULL != $useheight) {
-        $json = `${_BLOCKCHAIND} getblockbynumber ${useheight} true     `;
+    } else if (NULL != $usetx) {
+        $json = `${_BLOCKCHAIND} gettransaction ${usetx}`;
+        $input = json_decode($json);
     } else {
-        if (NULL == $useblock) {
-            $useblock = `${_BLOCKCHAIND} getbestblockhash  `;
+        if (NULL != $useheight) {
+            $json = `${_BLOCKCHAIND} getblockbynumber ${useheight} true     `;
+        } else {
+            if (NULL == $useblock) {
+                $useblock = `${_BLOCKCHAIND} getbestblockhash  `;
+            }
+            $json = `${_BLOCKCHAIND} getblock ${useblock}  true  `;
         }
-        $json = `${_BLOCKCHAIND} getblock ${useblock}  true  `;
+        $input = json_decode($json);
     }
-}
-$input = json_decode($json);
-
-#printf($json);
 ?>
 
 <table>
@@ -67,6 +79,11 @@ $input = json_decode($json);
     echo html_table_write($input, "toplevel");
     ?>
 </table>
+<?php
+echo "<p><a href=${_REURL}?top20=1> top 20 users</a>";
+echo "<p><a href=${_REURL}?total=1> total issued</a>";
+?>
+
 <?php
 function omitLabel($label)
 {
@@ -128,15 +145,15 @@ function html_table_write($inputObject, $label)
 
 function linkTo($label, $val)
 {
-
+    global $_REURL;
     $link = $val;
     switch ($label) {
         case "height":
-            $link = "<a href=" . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) . "?height=" . $val . ">$val</a>";
+            $link = "<a href=" . $_REURL . "?height=" . $val . ">$val</a>";
             break;
         case  "tx":
         case "txid":
-            $link = "<a href=" . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) . "?tx=$val title=$val>" . substr($val, 0, 8) . "</a>";
+            $link = "<a href=" . $_REURL . "?tx=$val title=$val>" . substr($val, 0, 8) . "</a>";
             break;
         case "proofhash":
         case "merkleroot":
@@ -147,10 +164,10 @@ function linkTo($label, $val)
         case "nextblockhash":
         case "blockhash":
         case "hash":
-            $link = "<a href=" . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) . "?block=$val title=$val>" . substr($val, 0, 8) . "</a>";
+            $link = "<a href=" . $_REURL . "?block=$val title=$val>" . substr($val, 0, 8) . "</a>";
             break;
         case "addresses":
-            $link = "<a href=" . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) . "?address=" . $val . ">$val</a>";
+            $link = "<a href=" . $_REURL . "?address=" . $val . ">$val</a>";
             break;
         case "time":
             $link = gmdate("Y-m-d\TH:i:s\Z", $val);
