dbd47b1277cdf447ae423281c23c6aeabb35d67e
Jim Northrup <jnorthrup@users.noreply.github.com>
2018-02-02 19:52:42 +0700

    Origin/extn (#1)
    * regex replacements.
    
    * init script for views.
    
    * init script for views.
    
    * ...
    
    * dos2unix

--- top20.sh
+++ top20.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-jq "[limit(20;.rows|sort_by(.value)|reverse[])]|from_entries" < <(curl "http://localhost:5984/sbit_blocks/_design/address/_view/value?group=true&reduce=true" )
+jq "[limit(20;.rows|sort_by(.value)|reverse[])]|from_entries" < <(curl "http://localhost:5984/extn_blocks/_design/address/_view/value?group=true&reduce=true" )
